515b1e6dd9f79039218844a37657936210fde24b
Zohiu <mail@zohiu.de>
2025-05-24 22:42:36 +0200

    Add License

--- run.bat
+++
@@ -1,2 +0,0 @@
-call .venv\Scripts\activate.bat
-flask --app server run --debug
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-gunicorn --reload --bind=127.0.0.1 -w 1 --worker-class=gevent --worker-connections=1000 'server:app' --timeout 300
+gunicorn --bind=127.0.0.1 -w 1 --worker-class=gevent --worker-connections=1000 'server:app' --timeout 300
 
 # Run caddy for https dev environment:
 # sudo caddy reverse-proxy -i --from localhost --to 127.0.0.1:8000

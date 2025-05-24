2ba6211abd3d9480568c66f1baa0ebfa5a884bf1
Zohiu <mail@zohiu.de>
2025-05-25 01:03:42 +0200

    Mozilla sw

--- web/sw.js
+++ web/sw.js
@@ -1,40 +1,45 @@
-var cacheName = 'websave-v1';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/lib/css/style.css',
-  '/lib/js/main.js',
-  '/lib/img/icon.png',
-  '/favicon.png',
-];
+const cacheName = 'websave-v1';
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
-  e.waitUntil(
-    (async () => {
-      const cache = await caches.open(cacheName);
-      console.log("[Service Worker] Caching all: app shell and content");
-      await cache.addAll(contentToCache);
-    })(),
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(resources);
+};
+
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      '/lib/css/style.css',
+      '/lib/js/main.js',
+      '/lib/img/icon.png',
+      '/favicon.png',
+    ])
   );
 });
 
 
-/* Serve cached content when offline */
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
-      }
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })(),
-  );
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async (request, event) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  const responseFromNetwork = await fetch(request);
+  event.waitUntil(putInCache(request, responseFromNetwork.clone()));
+  return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirst(event.request, event));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.registration?.navigationPreload.enable());
 });

bc3c7fca3571b827589da28aa3b65458ce5e8757
Zohiu <mail@zohiu.de>
2025-05-25 00:52:44 +0200

    SW logs

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'websave-v1';
 var filesToCache = [
   '/',
   '/index.html',
@@ -9,20 +9,32 @@ var filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })(),
   );
-  self.skipWaiting();
 });
 
+
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })(),
   );
 });
